8ffa23834b5c7101d0cbf13d342ab07606c98606
lauravsilva <lvs1646@rit.edu>
2016-10-20 19:41:37 -0400

    Created Hook files

---
+++ api/controllers/HookController.js
@@ -0,0 +1,11 @@
+/**
+ * HookController
+ *
+ * @description :: Server-side logic for managing Hooks
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
