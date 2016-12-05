a32921f5bdce09092e98bafbb7c7b3f96042b53b
Joey Bovio <bovio.joey@gmail.com>
2016-12-05 08:29:00 -0500

    Simplified necessary code

--- config/waterlock.js
+++ config/waterlock.js
@@ -8,19 +8,6 @@
  * http://waterlock.ninja/documentation
  */
 module.exports.waterlock = {
-
-  // Base URL
-  //
-  // used by auth methods for callback URI's using oauth and for password
-  // reset links.
-  baseUrl: 'http://facilities344.herokuapp.com',
-
-  // Auth Method(s)
-  //
-  // this can be a single string, an object, or an array of objects for your
-  // chosen auth method(s) you will need to see the individual module's README
-  // file for more information on the attributes necessary. This is an example
-  // of the local authentication method with password reset tokens disabled.
   authMethod: [
     {
        name: "waterlock-google-auth",
@@ -29,26 +16,16 @@ module.exports.waterlock = {
     }
   ],
 
-
-  // JSON Web Tokens
-  //
-  // this provides waterlock with basic information to build your tokens,
-  // these tokens are used for authentication, password reset,
-  // and anything else you can imagine
   jsonWebTokens:{
-    secret: process.env.JWT_SECRET,
+    secret: 'secret',//process.env.JWT_SECRET,
     expiry:{
       unit: 'hours',
       length: '1'
     },
-    audience: process.env.APP_NAME,
+    audience: 'Facilities 344',
     subject: 'subject',
-    trackUsage: true,
-    stateless: false,
-    tokenProperty: 'token',
-    expiresProperty: 'expires',
-    includeUserInJwtResponse: true
   },
+
   postActions: {
     login: {
       success: '/dashboard',
